6511c0035cb21622560747d3434c086d98c230bc
Lukas Möller <lmoeller@student.ethz.ch>
2021-02-07 13:10:17 +0100

    Add semantic release

--- test/viewport.test.tsx
+++ test/viewport.test.tsx
@@ -1,15 +1,12 @@
+import { render } from "@testing-library/react";
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import { PdfViewport } from "../src";
-import { portraitA4 } from "../src/components/viewport";
 
 describe("it", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(
-      <PdfViewport pdf={undefined} aspectRatio={portraitA4} pageNumber={1} />,
-      div
+  it("renders without crashing", async () => {
+    const { container } = render(
+      <PdfViewport pdf={undefined} pageNumber={1} aspectRatio={1}></PdfViewport>
     );
-    ReactDOM.unmountComponentAtNode(div);
+    const contentWrapper = container.querySelector("div > div > div");
   });
 });
