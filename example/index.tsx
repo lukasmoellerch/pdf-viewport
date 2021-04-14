d3566ba1082fffa8cf4ea42b65c571f638ac04e5
Lukas Möller <lmoeller@student.ethz.ch>
2021-04-14 15:19:34 +0200

    fix(example): use flex-wrap for side-by-side comparison

--- example/index.tsx
+++ example/index.tsx
@@ -58,14 +58,9 @@ const exampleD = `<PdfViewport
 </PdfViewport>`;
 
 const App = () => {
-  const [pdf, setPdf] = React.useState<PDFDocumentProxy | undefined>(undefined);
+  const [pdf, setPdf] = React.useState<PDFDocumentProxy | undefined>();
   React.useEffect(() => {
-    getDocument(examplePdf).promise.then(document => {
-      const pdf = document;
-
-      setPdf(pdf);
-      console.log(pdf);
-    });
+    getDocument(examplePdf).promise.then(setPdf);
   }, []);
   const pageStyle = tw`shadow-lg rounded-md overflow-hidden border-1 border-gray-400`;
   const canvasStyle = tw`rounded-md overflow-hidden`;
@@ -143,9 +138,9 @@ const App = () => {
       <h3 className={tw`text-lg font-bold mt-6 mb-4`}>
         Canvas &amp; SVG Rendering
       </h3>
-      <div className={tw`grid grid-cols-1 xl:grid-cols-2 gap-3`}>
+      <div className={tw`flex flex-wrap w-100`}>
         <div
-          className={tw`flex bg-purple-100 justify-between p-3 rounded-lg border-2 border-purple-200`}
+          className={tw`flex flex-grow bg-purple-100 justify-between p-3 items-start rounded-lg border-2 border-purple-200`}
         >
           <div className={tw`flex flex-col flex-grow`}>
             <div
@@ -154,33 +149,33 @@ const App = () => {
               SVG
             </div>
             <div
-              className={tw`flex flex-grow items-center text-purple-800 my-5`}
+              className={tw`flex flex-grow items-center text-purple-800 my-5 p-2`}
             >
               <pre className={tw`mx-auto text-xs sm:text-sm md:text-lg`}>
                 {exampleA}
               </pre>
             </div>
           </div>
-          <PdfViewport
-            aspectRatio={portraitA4}
-            pdf={pdf}
-            pageNumber={1}
+          <div
             className={pageStyle + " " + tw`bg-white w-1/3 md:w-1/2`}
+            style={{ minWidth: "12em" }}
           >
-            <PdfSvgLayer />
-          </PdfViewport>
+            <PdfViewport aspectRatio={portraitA4} pdf={pdf} pageNumber={1}>
+              <PdfSvgLayer />
+            </PdfViewport>
+          </div>
         </div>
         <div
-          className={tw`flex bg-green-100 justify-between p-3 rounded-lg border-2 border-green-200`}
+          className={tw`flex flex-grow bg-green-100 justify-between p-3 items-start rounded-lg border-2 border-green-200`}
         >
-          <PdfViewport
-            aspectRatio={portraitA4}
-            pdf={pdf}
-            pageNumber={1}
+          <div
             className={pageStyle + " " + tw`bg-white w-1/3 md:w-1/2`}
+            style={{ minWidth: "12em" }}
           >
-            <PdfCanvasLayer className={canvasStyle} />
-          </PdfViewport>
+            <PdfViewport aspectRatio={portraitA4} pdf={pdf} pageNumber={1}>
+              <PdfCanvasLayer className={canvasStyle} />
+            </PdfViewport>
+          </div>
           <div className={tw`flex flex-col flex-grow`}>
             <div
               className={tw`text-center ml-auto bg-green-500 rounded-lg py-2 px-5 text-green-100 font-bold`}
@@ -188,7 +183,7 @@ const App = () => {
               Canvas
             </div>
             <div
-              className={tw`flex flex-grow items-center text-green-800 my-5`}
+              className={tw`flex flex-grow items-center text-green-800 my-5 p-2`}
             >
               <pre className={tw`mx-auto text-xs sm:text-sm md:text-lg`}>
                 {exampleB}
