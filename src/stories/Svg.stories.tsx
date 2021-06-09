1fec50188c1f5c99fc3b892c11bc5a6f55a2be3c
Lukas Möller <lmoeller@student.ethz.ch>
2021-06-09 11:06:03 +0200

    Add storybook

---
+++ src/stories/Svg.stories.tsx
@@ -0,0 +1,43 @@
+import { Meta, Story } from "@storybook/react";
+import * as pdfjsLib from "pdfjs-dist/es5/build/pdf";
+import { getDocument } from "pdfjs-dist/es5/build/pdf";
+import { PDFDocumentProxy } from "pdfjs-dist/types/display/api";
+import React, { useEffect, useState } from "react";
+import { PdfViewport, PdfSvgLayer } from "..";
+import examplePdf from "./assets/pdfjs_example.pdf";
+
+export default {
+  title: "PdfViewport/Svg",
+  component: PdfSvgLayer,
+  argTypes: {
+    maxWidth: {
+      defaultValue: 500,
+      control: { type: "range", min: 100, max: 1500, step: 10 },
+    },
+  },
+} as Meta;
+
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.7.570/pdf.worker.min.js";
+
+const portraitA4 = 0.772727273;
+
+const Template: Story<{ maxWidth: number }> = ({ maxWidth }) => {
+  const [pdf, setPdf] = useState<PDFDocumentProxy>();
+  useEffect(() => {
+    getDocument(examplePdf).promise.then(setPdf);
+  }, []);
+  return (
+    <PdfViewport
+      aspectRatio={portraitA4}
+      pdf={pdf}
+      pageNumber={3}
+      style={{ maxWidth, margin: "auto", border: "1px solid black" }}
+    >
+      <PdfSvgLayer />
+    </PdfViewport>
+  );
+};
+
+export const Svg = Template.bind({});
+Svg.args = {};
