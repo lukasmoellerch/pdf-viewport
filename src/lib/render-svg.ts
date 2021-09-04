48ba4956681c4feb04606086661ea9576ca29c34
Lukas Möller <lmoeller@student.ethz.ch>
2021-09-04 19:32:41 +0200

    Add dark-mode svg implementation

--- src/lib/render-svg.ts
+++ src/lib/render-svg.ts
@@ -1,4 +1,5 @@
 import { PDFDocumentProxy } from "pdfjs-dist/types/display/api";
+import { darkModeTransform } from "./dark-mode";
 import { getGfx, getOperatorList, getPage } from "./promise-memo";
 import { getOrDefault } from "./utils";
 
@@ -7,13 +8,38 @@ const svgMap = new WeakMap<
   Map<number, Promise<SVGElement>>
 >();
 
+const id = <T>(x: T) => x;
+
+const applyColorTransform = (
+  rootElement: SVGElement,
+  fn: (color: string) => string = id
+) => {
+  rootElement.style.fill = fn("#000000");
+  rootElement.style.backgroundColor = fn("#ffffff");
+  if (fn === id) return;
+
+  const attributes = ["fill", "stroke"] as const;
+  for (const attribute of attributes) {
+    rootElement.querySelectorAll(`[${attribute}]`).forEach(element => {
+      const attributeValue = element.getAttribute(attribute);
+      if (attributeValue === "none") return;
+      if (attributeValue === undefined || attributeValue === null) return;
+      element.setAttribute(attribute, fn(attributeValue));
+    });
+  }
+};
+
 /**
  * Renders the page `pageNumber` to an SVGElement. The returned instance will
  * be unique and the caller is free to mount it anywhere in the tree.
  * @param pdf
  * @param pageNumber
  */
-export const renderSVG = async (pdf: PDFDocumentProxy, pageNumber: number) => {
+export const renderSVG = async (
+  pdf: PDFDocumentProxy,
+  pageNumber: number,
+  darkMode: boolean = false
+) => {
   const cache = getOrDefault(
     svgMap,
     pdf,
@@ -21,8 +47,15 @@ export const renderSVG = async (pdf: PDFDocumentProxy, pageNumber: number) => {
   );
 
   const cachedSvg = cache.get(pageNumber);
-  if (cachedSvg !== undefined)
-    return (await cachedSvg).cloneNode(true) as SVGElement;
+  if (cachedSvg !== undefined) {
+    const element = (await cachedSvg).cloneNode(true) as SVGElement;
+    if (darkMode) {
+      applyColorTransform(element, darkModeTransform);
+    } else {
+      applyColorTransform(element);
+    }
+    return element;
+  }
 
   const promise = (async () => {
     const page = await getPage(pdf, pageNumber);
@@ -34,5 +67,14 @@ export const renderSVG = async (pdf: PDFDocumentProxy, pageNumber: number) => {
 
   cache.set(pageNumber, promise);
 
-  return promise;
+  return promise.then(el => {
+    if (darkMode) {
+      const retEl = el.cloneNode(true) as SVGElement;
+      applyColorTransform(retEl, darkModeTransform);
+      return retEl;
+    } else {
+      applyColorTransform(el);
+      return el;
+    }
+  });
 };
